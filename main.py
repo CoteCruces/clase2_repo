097a8e47041a062fc54706ef58cb53664492c08c
cote <cote.cruces@gmail.com>
2025-06-13 15:42:29 -0400

    nueva calculadora

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 
-print("Hola 1")
-print("Hola 2")
-print("Hola 3")
+print("Calculadora")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Salir")
 
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
-
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
-
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
+while True:
+    opcion = int(input("Ingrese la opcion: "))
+    num1 = int(input("Ingrese numero 1: "))
+    num2 = int(input("Ingrese numero 2: "))
+    if opcion == 1:
+        resultado = num1 + num2
+        print(f"El resultado es {resultado}")
+    
+    elif opcion == 2:
+        resultado = num1 - num2
+        print(f"El resultado es {resultado}")
+    
+    elif opcion == 5:
+        break
+print("Gracias")
